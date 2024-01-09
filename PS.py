166ca33e85d8446b9e7fc61f3f3792bf4c95b0ca
gian_from_home <g.a.catalano@stir.ac.uk>
2024-01-09 11:32:25 +0000

    Additional implementation of PS, and some tests.

--- PS.py
+++ PS.py
@@ -16,7 +16,6 @@ class PS(Sized):
         self.fixed_mask = frozenbitarray(fixed_mask)
         self.values = np.fromiter(values, dtype=int)
 
-
     def __len__(self):
         return len(self.fixed_mask)
 
@@ -24,12 +23,7 @@ class PS(Sized):
         def repr_single(cell_value: int, is_fixed: bool) -> str:
             return f'{cell_value}' if is_fixed else '*'
 
-        return "["+"".join(map(repr_single, self.fixed_mask, self.values))
-
-
-    def __len__(self):
-        return len(self.fixed_mask)
-
+        return "[" + "".join(map(repr_single, self.fixed_mask, self.values))
 
     @classmethod
     def empty(cls, search_space: SearchSpace) -> Self:
@@ -39,7 +33,6 @@ class PS(Sized):
         irrelevant_values = np.zeros(len(search_space), dtype=int)
 
         return cls(no_fixed_values, irrelevant_values)
-
 
     def is_fully_fixed(self) -> bool:
         return self.fixed_mask.all()
@@ -53,4 +46,64 @@ class PS(Sized):
         all_fixed.setall(True)
         return cls(all_fixed, fs.values)
 
+    def with_unfixed_value(self, variable_position: int) -> Self:
+        new_fixed_mask = bitarray(self.fixed_mask)
+        new_fixed_mask[variable_position] = False
+        return PS(new_fixed_mask, self.values)
 
+    def with_fixed_value(self, variable_position: int, fixed_value: int) -> Self:
+        new_fixed_mask = bitarray(self.fixed_mask)
+        new_fixed_mask[variable_position] = True
+        new_values = np.array(self.values)
+        new_values[variable_position] = fixed_value
+        return PS(new_fixed_mask, new_values)
+
+    def get_fixed_variable_positions(self) -> list[int]:
+        return [position for position, is_used in enumerate(self.fixed_mask) if is_used]
+
+    def get_unfixed_variable_positions(self) -> list[int]:
+        return [position for position, is_used in enumerate(self.fixed_mask) if not is_used]
+
+    def simplifications(self) -> list[Self]:
+        return [self.with_unfixed_value(i) for i in self.get_fixed_variable_positions()]
+
+    def specialisations(self, search_space: SearchSpace) -> list[Self]:
+        return [self.with_fixed_value(position, value)
+                for position in self.get_unfixed_variable_positions()
+                for value in range(search_space.cardinalities[position])]
+
+    def __eq__(self, other: Self) -> bool:
+        """ This had to be optimised"""
+        if self.fixed_mask != other.fixed_mask:
+            return False
+
+        # shortcircuit on completely set features
+        if self.fixed_mask.all():
+            return np.array_equal(self.values, other.values_mask)
+
+        # full equality check
+        for value_here, value_there, is_set in zip(self.values, other.values_mask, self.fixed_mask):
+            if is_set and value_here != value_there:
+                return False
+
+        return True
+
+
+    @classmethod
+    def mergeable(cls, a: Self, b: Self) -> bool:
+        """In the places where both PSs have fixed params, the values are the same"""
+        for v_a, v_b, f_a, f_b in zip(a.values, b.values, a.fixed_mask, b.fixed_mask):
+            if (f_a and f_b) and (v_a != v_b):
+                return False
+        return True
+
+
+    @classmethod
+    def merge(cls, a: Self, b: Self) -> Self:
+        """assumes that mergeable(a, b) == True"""
+        merged_fixed_mask = a.fixed_mask | b.fixed_mask
+        merged_values = np.array(a.values)
+        for fixed_par_in_b in b.get_fixed_variable_positions():
+            merged_values[fixed_par_in_b] = b.values[fixed_par_in_b]
+
+        return cls(merged_fixed_mask, merged_values)
--- UnitTests/TestPS.py
+++ UnitTests/TestPS.py
@@ -1,11 +1,49 @@
 import unittest
 
 import numpy as np
+from bitarray import bitarray
 
 from FullSolution import FullSolution
+from PS import PS
 from SearchSpace import SearchSpace
 
 
 class TestPS(unittest.TestCase):
-    def test_bogus(self):
-        self.assertTrue(True, "is true")
+    def test_init(self):
+
+        mask = bitarray([True, False, True])
+        init_tuple = (0, 1, 2)
+        init_list = list(init_tuple)
+        init_ndarray = np.array(init_tuple)
+        init_gen = range(3)
+
+        from_tuple = PS(mask, init_tuple)
+
+        def assertCorrect(initialiser, msg):
+            new_ps = PS(mask, initialiser)
+            self.assertEqual(from_tuple, new_ps, msg)
+
+        assertCorrect(init_list, 'Initialiser from list did not match')
+        assertCorrect(init_ndarray, 'Initialiser from ndarray did not match')
+        assertCorrect(init_gen, 'Initialiser from generator did not match')
+
+    def test_empty(self):
+        search_space = SearchSpace((2, 3, 4))
+        empty = PS.empty(search_space)
+
+
+        self.assertEquals(len(empty), len(search_space), 'Empty PS has invalid length')
+        self.assertTrue(not empty.fixed_mask.any(), 'Empty PS has invalid mask')
+
+
+    def test_from_and_toFS(self):
+        fs = FullSolution((2, 3, 4))
+        ps = PS.from_FS(fs)
+
+        self.assertTrue(ps.fixed_mask.all(), 'PS.from_FS yielded a FS with an invalid mask')
+        self.assertTrue(np.array_equal(ps.values, fs.values), 'PS.from_FS yielded a FS with mismatched values')
+
+
+        self.assertTrue(ps.is_fully_fixed(), 'PS.from_FS(fs).is_fully_fixed results incorrect')
+
+
