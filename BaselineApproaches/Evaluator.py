cd706e834e3a065b3b1e84e0ea7dae5e8a2abe14
Giancarlo_Catalano <g.a.catalano@stir.ac.uk>
2024-02-06 16:18:03 +0000

    It appears that caching the PS is actually much slower

--- BaselineApproaches/Evaluator.py
+++ BaselineApproaches/Evaluator.py
@@ -46,20 +46,16 @@ class PSEvaluator(Evaluator):
     metrics: list[Metric]
     pRef: PRef
 
-    cached_metrics: dict[PS, MetricValues]
-
     def __init__(self, metrics: list[Metric], pRef: PRef):
         super().__init__()
         self.metrics = metrics
         self.pRef = pRef
-        self.cached_metrics = dict()
 
     # a population contains just partial solutions
     # a raw evaluated population contains partial solutions and some scores derived from the metrics
     # a normalised evaluated population contains PSs and a single float
 
-    def evaluate_fresh_population_with_raw_scores(self, pss: list[PS]) -> list[(PS, MetricValues)]:
-        # TODO: consider if this is cheating
+    def evaluate_population_with_raw_scores(self, pss: list[PS]) -> list[(PS, MetricValues)]:
         self.used_evaluations += len(pss)
 
         # this is a matrix
@@ -69,23 +65,7 @@ class PSEvaluator(Evaluator):
 
         pss_with_tuples = list(zip(pss, metric_tuples))
 
-        # update cache
-        self.cached_metrics.update(pss_with_tuples)
         return pss_with_tuples
-
-    def evaluate_population_with_raw_scores(self, pss: list[PS]) -> list[(PS, MetricValues)]:
-        """ Note that the partial solutions returned by this function are NOT in the same order as the input"""
-        #return self.evaluate_fresh_population_with_raw_scores(pss)  # EXPERIMENTAL
-        fresh_pss: list[PS] = []
-        stored_pss: list[(PS, MetricValues)] = []
-        for ps in pss:
-            cached_value = self.cached_metrics.get(ps)
-            if cached_value is None:
-                fresh_pss.append(ps)
-            else:
-                stored_pss.append((ps, cached_value))
-        evaluated_fresh_pss = self.evaluate_fresh_population_with_raw_scores(fresh_pss)
-        return evaluated_fresh_pss + stored_pss
 
     def evaluate_population(self, pss: list[PS]) -> list[(PS, float)]:
         with_raw_scores = self.evaluate_population_with_raw_scores(pss)
