dd562016c8f8aa0789d8f9ed22d84a74da23cc4e
Giancarlo_Catalano <g.a.catalano@stir.ac.uk>
2024-02-09 16:46:42 +0000

    Managed to get the integer problem to work.

--- JMetal/TestProblem.py
+++ JMetal/TestProblem.py
@@ -1,11 +1,14 @@
+import copy
 import random
 from abc import ABC
 
 import numpy as np
 from jmetal.algorithm.multiobjective import NSGAII
+from jmetal.core.operator import Crossover
 from jmetal.core.problem import Problem, BinaryProblem, IntegerProblem
 from jmetal.core.solution import BinarySolution, IntegerSolution
-from jmetal.operator import PolynomialMutation, SBXCrossover, SPXCrossover, BitFlipMutation
+from jmetal.operator import PolynomialMutation, SBXCrossover, SPXCrossover, BitFlipMutation, IntegerPolynomialMutation
+from jmetal.operator.crossover import IntegerSBXCrossover
 from jmetal.util.solution import get_non_dominated_solutions
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
@@ -92,19 +95,63 @@ class GianIntegerProblem(IntegerProblem, ABC):
         solution.objectives[1] = score_product
         return solution
 
-
     def create_solution(self) -> IntegerSolution:
         new_solution = IntegerSolution(
             self.lower_bound,
             self.upper_bound,
             self.number_of_objectives(),
             self.number_of_constraints())
-        new_solution.variables[0] = [random.randrange(lower, upper) for lower, upper in zip(self.lower_bound, self.upper_bound)]
+        new_solution.variables[0] = [random.randrange(lower, upper) for lower, upper in
+                                     zip(self.lower_bound, self.upper_bound)]
 
         return new_solution
 
     def name(self) -> str:
         return "Subset Sum"
+
+
+class BoringIntegerProblem(IntegerProblem, ABC):
+    lower_bound = [2, 3, 4]
+    upper_bound = [8, 9, 10]
+
+    def __init__(self):
+        super(BoringIntegerProblem, self).__init__()
+
+        self.obj_directions = [self.MINIMIZE, self.MAXIMIZE]
+        self.obj_labels = ["Sum", "Product"]
+        self.lower_bound = [2, 3, 4]
+        self.upper_bound = [8, 9, 10]
+
+    def number_of_constraints(self) -> int:
+        return 0
+
+    def number_of_objectives(self) -> int:
+        return 2
+
+    def number_of_variables(self) -> int:
+        return 1
+
+    def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
+        score_sum = sum(solution.variables)
+        score_product = np.product(solution.variables)
+        solution.objectives[0] = score_sum
+        solution.objectives[1] = score_product
+        return solution
+
+    def create_solution(self) -> IntegerSolution:
+        new_solution = IntegerSolution(
+            lower_bound=self.lower_bound,
+            upper_bound=self.upper_bound,
+            number_of_objectives=self.number_of_objectives(),
+            number_of_constraints=self.number_of_constraints())
+
+        new_solution.variables = [random.randrange(lower, upper)
+                                     for lower, upper in zip(self.lower_bound, self.upper_bound)]
+
+        return new_solution
+
+    def name(self) -> str:
+        return "Boring Integer problem"
 
 
 def test_JMetal_1():
@@ -115,7 +162,7 @@ def test_JMetal_1():
         population_size=100,
         offspring_population_size=100,
         mutation=BitFlipMutation(probability=1 / problem.number_of_bits),
-        crossover=SPXCrossover(probability=1),
+        crossover=IntegerSBXCrossover(probability=1, distribution_index = 20),
         termination_criterion=StoppingByEvaluations(max_evaluations=10000))
 
     print("Setup the algorithm, now we run it")
@@ -132,13 +179,13 @@ def test_JMetal_1():
 
 
 def test_JMetal_integer():
-    problem = GianIntegerProblem(SearchSpace([2, 3, 4]))
+    problem = BoringIntegerProblem()
     algorithm = NSGAII(
         problem=problem,
         population_size=100,
         offspring_population_size=100,
-        mutation=PolynomialMutation(probability=1 / problem.search_space.amount_of_parameters, distribution_index=20),
-        crossover=SBXCrossover(probability=1 / problem.search_space.amount_of_parameters, distribution_index=20),
+        mutation=IntegerPolynomialMutation(probability=1 / 3, distribution_index=20),
+        crossover=IntegerSBXCrossover(probability=1, distribution_index = 20),
         termination_criterion=StoppingByEvaluations(max_evaluations=10000))
 
     print("Setup the algorithm, now we run it")
